7ff58d4f640e945528fce4b4d602910bdfdae8d5
Sebastian Golian <sebastiangolian@gmail.com>
2020-01-29 10:48:56 +0100

    add prime-ng package

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
@@ -6,6 +7,7 @@ import { PageHomeComponent } from './components/page-home/page-home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
 
 
 @NgModule({
@@ -15,10 +17,12 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    MenubarModule
   ],
   providers: [
 
--- src/app/app/components/app/app.component.ts
+++ src/app/app/components/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-prime-ng';
+export class AppComponent implements OnInit {
+    title = 'angular-prime-ng';
+    items: MenuItem[];
+
+    ngOnInit() {
+        this.items = [
+            {label: 'Home', routerLink: 'home'},
+            {label: 'Test', routerLink: 'page-form'}
+        ];
+    }
 }
